48c21dec443354c0ff934b2128cb7ced769cfa09
Daniel Platon <platon@adobe.com>
2017-06-30 00:01:15 +0300

    Add small comment

--- webpack.config.js
+++ webpack.config.js
@@ -57,5 +57,6 @@ module.exports = {
             path.resolve(__dirname, './node_modules/foundation-sites/scss')
         ]
     },
-    devtool: process.env.NODE_ENV == 'production' ? undefined : 'cheap-module-eval-source-map'
+    /* leave sourcemaps off for production */
+    devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
 }
